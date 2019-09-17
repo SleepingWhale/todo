a5189ee68a61166db566b3f20cb84d0ec75e307b
Vladimir Vakhromeev <VladimirVakhromeev@mysale.com>
2019-09-17 14:39:43 +0300

    Add utils

---
+++ src/utils.js
@@ -0,0 +1,13 @@
+function s4() {
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+}
+
+export const getGuid = () =>
+  `${s4() + s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
+
+export const pluralize = (word, count) => (count === 1 ? word : `${word}s`);
+
+export const ESCAPE_KEY = 27;
+export const ENTER_KEY = 13;
